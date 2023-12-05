d59f46eadad33c8428df1ca8a3cff5803aca6a39
Ash <ashwinirao.karai@gmail.com>
2023-12-04 23:53:42 -0700

    builder exercise-2: design fields as required or optional

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -3,44 +3,82 @@ package serenitylabs.tutorials.vetclinic.domain;
 import java.time.LocalDateTime;
 
 public class Dog {
-    private String name, breed, dateOfBirth ;
-    public Dog(String name, String breed, String dateOfBirth) {
+    private String name, breed, dateOfBirth, color, optionalFavoriteFood, optionalFavoriteToy;
+    public Dog(String name, String breed, String color, String optionalFavoriteFood, String optionalFavoriteToy, String dateOfBirth) {
         this.name = name;
         this.breed = breed;
+        this.color = color;
+        this.optionalFavoriteFood = optionalFavoriteFood;
+        this.optionalFavoriteToy = optionalFavoriteToy;
         this.dateOfBirth = dateOfBirth;
     }
 
     public String getName(){
         return name;
     }
+    public String getBreed(){
+        return breed;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+   public String getOptionalFavoriteFood(){
+        return optionalFavoriteFood;
+   }
+
+    public String getOptionalFavoriteToy(){
+        return optionalFavoriteToy;
+    }
 
     public String getDateOfBirth(){
         return dateOfBirth;
     }
 
-    public String getBreed(){
-        return breed;
+    public interface WithBreed{
+        public WithColor ofBreed(String breed);
     }
 
-    public static DogBreeder called(String name) {
+    public interface WithColor{
+        public DogBreeder ofColor(String color);
+    }
+
+    //The first method the caller (test) calls to build a Dog object
+    public static WithBreed called(String name) {
         return new DogBreeder(name);
     }
 
-    public static class DogBreeder {
-        public String name, breed, dateOfBirth;
+    public static class DogBreeder implements WithBreed, WithColor{
+        public String name, breed, dateOfBirth, color, optionalFavoriteFood, optionalFavoriteToy;
 
         public DogBreeder(String name){
             this.name = name;
         }
 
-        public DogBreeder ofBreed(String breed){
+        public WithColor ofBreed(String breed){
             this.breed  = breed;
+            return this;
+        }
+
+        public DogBreeder ofColor(String color){
+            this.color = color;
+            return this;
+        }
+
+        public DogBreeder likesToEat(String optionalFavoriteFood){
+            this.optionalFavoriteFood = optionalFavoriteFood;
+            return this;
+        }
+
+        public DogBreeder likesToPlayWith(String optionalFavoriteToy){
+            this.optionalFavoriteToy = optionalFavoriteToy;
             return this;
         }
 
         public Dog bornOn(String dateOfBirth){
             this.dateOfBirth = dateOfBirth;
-            return new Dog(name, breed, dateOfBirth);
+            return new Dog(name, breed, color, optionalFavoriteFood, optionalFavoriteToy, dateOfBirth);
         }
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -6,24 +6,80 @@ import org.junit.Test;
 import java.time.LocalDateTime;
 
 public class WhenWeCreateANewDog {
-    @Test
-    public void a_new_dog_should_have_a_name(){
-        String dateOfBirth = "Dec-02-2023";
-        Dog fido = new Dog("fido", "labrador", dateOfBirth);
-       Assertions.assertThat(fido.getName()).isEqualTo("fido");
-       Assertions.assertThat(fido.getDateOfBirth()).isEqualTo(dateOfBirth);
-    }
+//    @Test
+//    public void a_new_dog_should_have_a_name(){
+//        String dateOfBirth = "Dec-02-2023";
+//        Dog fido = new Dog("fido", "labrador", dateOfBirth);
+//       Assertions.assertThat(fido.getName()).isEqualTo("fido");
+//       Assertions.assertThat(fido.getDateOfBirth()).isEqualTo(dateOfBirth);
+//    }
 
     @Test
-    public void can_create_a_dog_with_builder(){
+    public void can_create_a_dog_with_required_data_using_builder(){
        final  String THE_FOURTH_OF_JULY = "Jul-04-2023";
 
         Dog fido = Dog.called("Fido")
                                  .ofBreed("Labrador")
+                                .ofColor("black")
                                 .bornOn(THE_FOURTH_OF_JULY);
 
         Assertions.assertThat(fido.getName()).isEqualTo("Fido");
         Assertions.assertThat(fido.getBreed()).isEqualTo("Labrador");
+        Assertions.assertThat(fido.getColor()).isEqualTo("black");
         Assertions.assertThat(fido.getDateOfBirth()).isEqualTo(THE_FOURTH_OF_JULY);
     }
+
+    @Test
+    public void can_add_optional_favorite_food(){
+        final  String THE_FOURTH_OF_JULY = "Jul-04-2023";
+
+        Dog fido = Dog.called("Fido")
+                .ofBreed("Labrador")
+                .ofColor("black")  //caller can either call likesToEat before calling bornOn after setting color
+                .likesToEat("banana")
+                .bornOn(THE_FOURTH_OF_JULY);
+
+        Assertions.assertThat(fido.getName()).isEqualTo("Fido");
+        Assertions.assertThat(fido.getBreed()).isEqualTo("Labrador");
+        Assertions.assertThat(fido.getColor()).isEqualTo("black");
+        Assertions.assertThat(fido.getOptionalFavoriteFood()).isEqualTo("banana");
+        Assertions.assertThat(fido.getDateOfBirth()).isEqualTo(THE_FOURTH_OF_JULY);
+    }
+
+    @Test
+    public void can_add_optional_favorite_toy(){
+        final  String THE_FOURTH_OF_JULY = "Jul-04-2023";
+
+        Dog fido = Dog.called("Fido")
+                .ofBreed("Labrador")
+                .ofColor("black")   //caller can either call likesToPlayWith or likesToEat before calling  bornOn after setting color
+                .likesToPlayWith("ball")
+                .bornOn(THE_FOURTH_OF_JULY);
+
+        Assertions.assertThat(fido.getName()).isEqualTo("Fido");
+        Assertions.assertThat(fido.getBreed()).isEqualTo("Labrador");
+        Assertions.assertThat(fido.getColor()).isEqualTo("black");
+        Assertions.assertThat(fido.getOptionalFavoriteToy()).isEqualTo("ball");
+        Assertions.assertThat(fido.getDateOfBirth()).isEqualTo(THE_FOURTH_OF_JULY);
+    }
+
+    @Test
+    public void can_build_dog_without_optional_data(){
+        final  String THE_FOURTH_OF_JULY = "Jul-04-2023";
+
+        Dog fido = Dog.called("Fido")
+                .ofBreed("Labrador")
+                .ofColor("black")   //caller can either call likesToPlayWith or likesToEat or neither before calling  bornOn after setting color
+//                .likesToEat("banana")     //skip
+//                .likesToPlayWith("ball")  //skip
+                .bornOn(THE_FOURTH_OF_JULY);
+
+        Assertions.assertThat(fido.getName()).isEqualTo("Fido");
+        Assertions.assertThat(fido.getBreed()).isEqualTo("Labrador");
+        Assertions.assertThat(fido.getColor()).isEqualTo("black");
+        Assertions.assertThat(fido.getOptionalFavoriteFood()).isNull();
+        Assertions.assertThat(fido.getOptionalFavoriteToy()).isNull();
+        Assertions.assertThat(fido.getDateOfBirth()).isEqualTo(THE_FOURTH_OF_JULY);
+    }
+
 }
