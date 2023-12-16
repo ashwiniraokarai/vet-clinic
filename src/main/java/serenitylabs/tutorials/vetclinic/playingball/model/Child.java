b0f172ac8f5ca5cd6de9523ffa6ab322344d43e1
Ash <ashwinirao.karai@gmail.com>
2023-12-16 08:59:50 -0700

    strategy pattern: in the Child class, replace the fetchRightGame() with a factory that returns the matching implementation of the Strategy (Interface)

--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
@@ -1,22 +1,9 @@
 package serenitylabs.tutorials.vetclinic.playingball.model;
 
-import static serenitylabs.tutorials.vetclinic.playingball.model.Game.*;
-
 public class Child {
 
     public void goPlay(Game game){
-        Player player = fetchRightGame(game);
-        player.play();
-    }
-
-    public Player fetchRightGame(Game game){
-        switch(game) {
-            case Football: return new FootBallGame();
-            case Tennis: return new TennisGame();
-            case Cricket: return new CricketGame();
-            case Handball: return new HandBallGame();
-            case Hockey: return new HockeyGame();
-            default: throw new DontRecognizeThatGameException();
-        }
+      Player gameToPlay =  FetchGame.called(game);
+      gameToPlay.play();
     }
 }
--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/HandBallGame.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/HandBallGame.java
@@ -1,5 +1,5 @@
 package serenitylabs.tutorials.vetclinic.playingball.model;
-public class HandBallGame implements Player{
+public class HandBallGame implements Player {
     public void play(){
         System.out.print("Throw the ball");
     }
