ada61d247182cff80abb5e22f247c780ddbab2be
Ash <ashwinirao.karai@gmail.com>
2023-12-24 13:41:26 -0800

    try to revert tax rate calculation to repeat work

--- src/main/java/serenitylabs/tutorials/vetclinic/sales/SalesTaxService.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/sales/SalesTaxService.java
@@ -5,7 +5,9 @@ import serenitylabs.tutorials.vetclinic.sales.model.SalesTax;
 
 public class SalesTaxService {
 
-    public SalesTax salesTaxEntryFor(LineItem item) {
+    public SalesTax
+
+    salesTaxEntryFor(LineItem item) {
         return null;
     }
 }
