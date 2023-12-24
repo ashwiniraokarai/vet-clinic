47fcd78840350a896cc1eb83f6e96a25c6f4e090
Ash <ashwinirao.karai@gmail.com>
2023-12-24 13:34:08 -0800

    revert Tax Rate calculation for the remaining cases to repeat work

--- src/main/java/serenitylabs/tutorials/vetclinic/sales/TaxRateAssessor.java
+++
@@ -1,7 +0,0 @@
-package serenitylabs.tutorials.vetclinic.sales;
-
-import serenitylabs.tutorials.vetclinic.sales.model.LineItem;
-
-public interface TaxRateAssessor {
-    TaxRate rateFor(LineItem item);
-}
