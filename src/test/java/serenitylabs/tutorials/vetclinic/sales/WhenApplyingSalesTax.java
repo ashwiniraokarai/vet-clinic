b918a2643a6ee7c43c693d86395dd2d1f80480b0
Ash <ashwinirao.karai@gmail.com>
2023-12-24 13:30:21 -0800

    Revert strategy pattern to repeat work' 'Revert "strategy pattern: with functions: exercise of part 1: implement Tax Rate calculation for the remaining cases"
    This reverts commit c30802adb1b87f92e46f7f0ebcd34183cc3e38ef.

--- src/test/java/serenitylabs/tutorials/vetclinic/sales/WhenApplyingSalesTax.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/sales/WhenApplyingSalesTax.java
@@ -2,23 +2,19 @@ package serenitylabs.tutorials.vetclinic.sales;
 
 import org.junit.Test;
 import serenitylabs.tutorials.vetclinic.sales.model.LineItem;
-import serenitylabs.tutorials.vetclinic.sales.model.ProductCategory;
 import serenitylabs.tutorials.vetclinic.sales.model.SalesTax;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static serenitylabs.tutorials.vetclinic.sales.model.ProductCategory.Books;
 import static serenitylabs.tutorials.vetclinic.sales.model.ProductCategory.Snacks;
 
 public class WhenApplyingSalesTax {
 
-    private static final double NINE_PERCENT = 0.09;
-    private static final double THIRTEEN_POINT_FIVE_PERCENT = 0.135;
-    private static final  double ZERO_PERCENT = 0.00 ;
-    private static final double TWENTY_THREE_PERCENT = 00.23;
+    private final static double NINE_PERCENT = 0.09;
+    private final static double THIRTEEN_POINT_FIVE_PERCENT = 0.135;
 
     @Test
-    public void crisps_should_be_charged_at_the_reduced_tax_rate() {
+    public void crisps_should_be_charged_at_the_reduced_rate() {
         // GIVEN
         LineItem crisps = LineItem.forASaleOf(1)
                                        .itemCalled("salt and vinegar crisps")
@@ -37,7 +33,7 @@ public class WhenApplyingSalesTax {
     }
 
     @Test
-    public void over_100_euros_crisps_should_be_charged_at_a_higher_tax_rate() {
+    public void over_100_euros_crisps_should_be_charged_at_a_higher_rate() {
         // GIVEN
         LineItem crisps = LineItem.forASaleOf(50)
                 .itemCalled("salt and vinegar crisps")
@@ -50,42 +46,6 @@ public class WhenApplyingSalesTax {
 
         // THEN
         SalesTax expectedSalesTax = SalesTax.atRateOf(THIRTEEN_POINT_FIVE_PERCENT).withName("Reduced - Variant").forAnAmountOf(50*3*THIRTEEN_POINT_FIVE_PERCENT);
-
-        assertThat(calculatedSalesTax, equalTo(expectedSalesTax));
-    }
-
-    @Test
-    public void books_should_be_charged_zero_tax_rate() {
-        // GIVEN
-        LineItem crisps = LineItem.forASaleOf(10)
-                .itemCalled("the da vinci code")
-                .inCategory(Books)
-                .withAUnitPriceOf(10.00);
-
-        // WHEN
-        SalesTaxService salesTaxService = new SalesTaxService();
-        SalesTax calculatedSalesTax = salesTaxService.salesTaxEntryFor(crisps);
-
-        // THEN
-        SalesTax expectedSalesTax = SalesTax.atRateOf(ZERO_PERCENT).withName("Exempt").forAnAmountOf(10*10*ZERO_PERCENT);
-
-        assertThat(calculatedSalesTax, equalTo(expectedSalesTax));
-    }
-
-    @Test
-    public void toys_should_be_charged_standard_tax_rate() {
-        // GIVEN
-        LineItem crisps = LineItem.forASaleOf(10)
-                .itemCalled("lovey")
-                .inCategory(ProductCategory.Toys)
-                .withAUnitPriceOf(10.00);
-
-        // WHEN
-        SalesTaxService salesTaxService = new SalesTaxService();
-        SalesTax calculatedSalesTax = salesTaxService.salesTaxEntryFor(crisps);
-
-        // THEN
-        SalesTax expectedSalesTax = SalesTax.atRateOf(TWENTY_THREE_PERCENT).withName("Standard").forAnAmountOf(10*10*TWENTY_THREE_PERCENT);
 
         assertThat(calculatedSalesTax, equalTo(expectedSalesTax));
     }
